07e53d082fbfc3fe3eabdbdda3246fb14f0bb84c
youraunt <briceallen@gmail.com>
2020-05-10 13:39:57 -0600

    Final sans comments

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# inworks2300-allenBriceFinal" 
+"#H1 
+Brice Allen
+Dr. Goodman
+IWKS-2300
+Final
+" 
+>For this project I selected to visualize
+>the COVID-19 pandemic. I utilized python
+>as the main language and additionally 
+>integrated:
+>- Dash 
+>- matplotlib
+>- pandas
+>- numpy
+>
+>Below you will find screen shots. 
+>
--- main.py
+++ main.py
@@ -1,40 +1,102 @@
-#!/usr/bin/env python3
-
+#  Dependencies
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
-import os
+import numpy as np
 
-csv_url = 'https://raw.githubusercontent.com/youraunt/public/master/INWK2300/covid_19_Colorado_Daily.csv'
-df = pd.read_csv(csv_url, parse_dates=['Date'])
+#  urls used
+ny_times_url_state = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
+ny_times_url_county = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+my_git_url = 'https://raw.githubusercontent.com/youraunt/public/master/INWK2300/covid_19_Colorado_Daily.csv'
+colorado_gov_url = 'https://covid19.colorado.gov/case-data'
+welcome_message = '\n\nWelcome to my Data Visualization Project\n' \
+                  'Click the url to load in your browser.\n\n\n\n'
+df = pd.read_csv(my_git_url, parse_dates=['Date'])
+# -------------------------------------------------------------------------------------------------------------
+#  Data
+data_frame = pd.DataFrame(pd.read_csv(ny_times_url_state, parse_dates=['date']))
+data_frame = pd.pivot_table(data_frame, values='deaths', index=['date'], columns='state', aggfunc=np.sum)
+#  Working with go.Figure to handle style
+total_deaths_nytimes = go.Figure()
+for col in data_frame.columns:
+    total_deaths_nytimes.add_trace(
+        go.Scatter(x=data_frame.index, y=data_frame[col].values, name=col, mode='markers+lines',
+                   line=dict(shape='linear'), connectgaps=True))
+total_deaths_nytimes.update_layout(
+    title="Total Deaths",
+    xaxis_title='Date',
+    yaxis_title="# Deceased",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color='grey'
+    )
+)
+data_frame = pd.DataFrame(pd.read_csv(ny_times_url_state, parse_dates=['date']))
+data_frame = pd.pivot_table(data_frame, values='cases', index='date', columns='state', aggfunc=np.sum)
+total_cases_nytimes = go.Figure()
+
+for col in data_frame.columns:
+    total_cases_nytimes.add_trace(
+        go.Scatter(x=data_frame.index, y=data_frame[col].values, name=col, mode='markers+lines',
+                   line=dict(shape='linear'), connectgaps=True)
+    )
+total_cases_nytimes.update_layout(
+    title="Total Infections",
+    xaxis_title='Date',
+    yaxis_title="# Infected",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color='grey'
+    )
+)
+data_frame = pd.DataFrame(pd.read_csv(ny_times_url_county, parse_dates=['date']))
+data_frame = data_frame.loc[data_frame['state'] == 'Colorado']
+data_frame = pd.pivot_table(data_frame, values='deaths', index='date', columns='county', aggfunc=np.sum)
+death_by_county = go.Figure()
+
+for col in data_frame.columns:
+    death_by_county.add_trace(
+        go.Scatter(x=data_frame.index, y=data_frame[col].values, name=col, mode='markers+lines',
+                   line=dict(shape='linear'), connectgaps=True)
+    )
+death_by_county.update_layout(
+    title="Deaths by County",
+    xaxis_title='Date',
+    yaxis_title="# Deceased",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color='grey'
+    ))
+
+# -------------------------------------------------------------------------------------------------------------
 
 app = dash.Dash()
 server = app.server
 
-tested_bar_graph = go.Bar(x=df['Date'], y=df['Tested'], marker_color='CornflowerBlue')
 tested_scatter_graph = go.Scatter(x=df['Date'], y=df['Tested'], marker_color='darkSalmon')
 new_cases_scatter_graph = go.Scatter(x=df['Date'], y=df['New Cases'], mode='lines+markers', marker_color='darkSalmon',
                                      line_color='CornflowerBlue')
 new_deaths_graph = go.Scatter(x=df['Date'], y=df['New Deaths'], mode='lines+markers', marker_color='darkSalmon',
                               line_color='CornflowerBlue')
-confirmed_bar_graph = go.Bar(x=df['Date'], y=df['Confirmed'], marker_color='CornflowerBlue')
 confirmed_scatter_graph = go.Scatter(x=df['Date'], y=df['Confirmed'], marker_color='darkSalmon')
-hospital_bar_graph = go.Bar(x=df['Date'], y=df['Hospitalized'], marker_color='CornflowerBlue')
 hospital_scatter_graph = go.Scatter(x=df['Date'], y=df['Hospitalized'], marker_color='darkSalmon')
-death_bar_graph = go.Bar(x=df['Date'], y=df['Death'], marker_color='CornflowerBlue')
 death_scatter_graph = go.Scatter(x=df['Date'], y=df['Death'], marker_color='darkSalmon')
-
+#  Working with Dash to handle style
 app.layout = html.Div(
     style={'backgroundColor': 'white'},
     children=[
         html.H1(
             children=
-            'Graphing the the effects of COVID-19 in Colorado.',
+            'Graphing the the effects of COVID-19.',
             style={
                 'textAlign': 'center',
                 'color': 'black'
+
             }
         ),
         dcc.Graph(
@@ -47,10 +109,10 @@ app.layout = html.Div(
                         'showgrid': False
                     },
                     'yaxis': {
-                        'title': 'Number of Individuals',
+                        'title': '# Deceased',
                         'showgrid': False
                     },
-                    'title': 'New Deaths',
+                    'title': 'New Deaths by Day',
                     'font': {
                         'color': 'grey'
                     }
@@ -68,10 +130,10 @@ app.layout = html.Div(
                         'rotation': 45
                     },
                     'yaxis': {
-                        'title': 'Number of Individuals',
+                        'title': '# Cases',
                         'showgrid': False
                     },
-                    'title': 'New Cases',
+                    'title': 'New Cases by Day',
                     'font': {
                         'color': 'grey'
                     }
@@ -89,37 +151,16 @@ app.layout = html.Div(
                         'showgrid': False
                     },
                     'yaxis': {
-                        'title': 'Number of Individuals',
+                        'title': '# Tested',
                         'showgrid': False
                     },
-                    'title': 'Tested',
+                    'title': 'Total Tested',
                     'font': {
                         'color': 'grey'
                     }
                 }
             }
         ),
-        dcc.Graph(
-            id='Tested',
-            figure={
-                'data': [tested_bar_graph],
-                'layout': {
-                    'xaxis': {
-                        'title': 'Date',
-                        'showgrid': False
-                    },
-                    'yaxis': {
-                        'title': 'Number of Individuals',
-                        'showgrid': False
-                    },
-                    'title': 'Tested',
-                    'font': {
-                        'color': 'grey'
-                    }
-                }
-            }
-        ),
-
         dcc.Graph(
             id='Graph_confirmed',
             figure={
@@ -130,31 +171,10 @@ app.layout = html.Div(
                         'showgrid': False
                     },
                     'yaxis': {
-                        'title': 'Number of Individuals',
+                        'title': '# Confirmed',
                         'showgrid': False
                     },
-                    'title': 'Confirmed',
-                    'font': {
-                        'color': 'grey'
-                    }
-                }
-            }
-        ),
-
-        dcc.Graph(
-            id='Graph_Test_confirmed',
-            figure={
-                'data': [confirmed_bar_graph],
-                'layout': {
-                    'xaxis': {
-                        'title': 'Date',
-                        'showgrid': False
-                    },
-                    'yaxis': {
-                        'title': 'Number of Individuals',
-                        'showgrid': False
-                    },
-                    'title': 'Confirmed',
+                    'title': 'Total Confirmed Infections',
                     'font': {
                         'color': 'grey'
                     }
@@ -172,35 +192,17 @@ app.layout = html.Div(
                         'showgrid': False
                     },
                     'yaxis': {
-                        'title': 'Number of Individuals',
+                        'title': '# Hospitalized',
                         'showgrid': False
                     },
-                    'title': 'Hospitalized',
+                    'title': 'Total Hospitalized',
                     'font': {
                         'color': 'grey'
                     }
                 }
             }
         ),
-        dcc.Graph(
-            id='Graph_hospital',
-            figure={
-                'data': [hospital_bar_graph],
-                'layout': {
-                    'xaxis': {
-                        'title': 'Date',
-                        'showgrid': False
-                    },
-                    'yaxis': {
-                        'title': 'Number of Individuals',
-                        'showgrid': False
-                    },
-                    'font': {
-                        'color': 'grey'
-                    }
-                }
-            }
-        ),
+
         dcc.Graph(
             id='Graph_death',
             figure={
@@ -211,10 +213,10 @@ app.layout = html.Div(
                         'showgrid': False
                     },
                     'yaxis': {
-                        'title': 'Number of Individuals',
+                        'title': '# Deceased',
                         'showgrid': False
                     },
-                    'title': 'Deaths',
+                    'title': 'Total Deaths',
                     'font': {
                         'color': 'grey'
                     }
@@ -222,35 +224,31 @@ app.layout = html.Div(
             }
 
         ),
-        dcc.Graph(
-            id='Graph_Test_deaths',
-            figure={
-                'data': [death_bar_graph],
-                'layout': {
-                    'xaxis': {
-                        'title': 'Date',
-                        'showgrid': False
-                    },
-                    'yaxis': {
-                        'title': 'Number of Individuals',
-                        'showgrid': False
-                    },
-                    'title': 'Deaths',
-                    'font': {
-                        'color': 'grey'
-                    }
-                }
-            }
-        ),
+        dcc.Graph(id='times_by_state', figure=total_deaths_nytimes),
+        dcc.Graph(id='times_cases', figure=total_cases_nytimes),
+        dcc.Graph(id='death_by_county', figure=death_by_county),
         html.Div(
-            children='Retrieved From: https://raw.githubusercontent.com/youraunt/public/master/INWK2300'
-                     '/covid_19_Colorado_Daily.csv',
+            children='.............................',
+            style={'textAlign': 'left', 'color': 'white'
+                   }),
+        html.Div(
+            children='Source: ' + my_git_url,
             style={'textAlign': 'left', 'color': 'grey'
                    }),
         html.Div(
-            children='Source: https://covid19.colorado.gov/case-data',
+            children='Source: ' + colorado_gov_url,
+            style={'textAlign': 'left', 'color': 'grey'
+                   }),
+        html.Div(
+            children='Source: ' + ny_times_url_state,
+            style={'textAlign': 'left', 'color': 'grey'
+                   }),
+        html.Div(
+            children='Source: ' + ny_times_url_county,
             style={'textAlign': 'left', 'color': 'grey'
                    })
+
     ])
 if __name__ == '__main__':
+    print(welcome_message)
     app.run_server(debug=True)
